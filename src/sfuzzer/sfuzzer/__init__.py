0a801e080aa495b7a67efaafef9187cbba61ac2e
Shankar <123456789987654321sr@gmail.com>
2019-12-01 09:40:27 +0530

    deleted files

--- src/sfuzzer/sfuzzer/__init__.py
+++
@@ -1,8 +0,0 @@
-def fuzzint(argument):
-    def decorator(function):
-        def fuzz(*args,**kwargs):
-            print("inside decorator")
-            print("argument:"+str(argument))
-            return function(*args,**kwargs)
-        return fuzz
-    return decorator
